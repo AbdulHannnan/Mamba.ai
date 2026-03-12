e0ed280240136ac3eaa544f5483c390d902b1963
Abdul Hannan <ahk12067@gmail.com>
2026-03-13 04:11:10 +0500

    Setup the Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import Home from './pages/Home';
 import SoftBackdrop from './components/SoftBackdrop';
 import Footer from './components/Footer';
 import LenisScroll from './components/lenis';
+import { Route, Routes } from 'react-router-dom';
+import Generator from './pages/Generator';
+import Community from './pages/Community';
+import Results from './pages/Results';
+import MyGeneration from './pages/MyGeneration';
+import Plans from './pages/Plans';
+import Loading from './pages/Loading';
 
 function App() {
 	return (
@@ -10,7 +17,17 @@ function App() {
 			<SoftBackdrop />
 			<LenisScroll />
 			<Navbar />
-			<Home />
+			<Routes>	
+			<Route path="/" element={<Home />} />
+			<Route path="/generate" element={<Generator />} />
+			<Route path="/community" element={<Community />} />
+			<Route path="/results:projectId" element={<Results />} />
+			<Route path="/mygenerations" element={<MyGeneration />} />
+			<Route path="/plans" element={<Plans />} />
+			<Route path="/loading" element={<Loading />} />
+			
+			
+			</Routes>
 			<Footer />
 		</>
 	);
