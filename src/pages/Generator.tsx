8af345ce8a1f72250255ab2651000a2b4ecc727e
Abdul Hannan <ahk12067@gmail.com>
2026-03-15 22:08:02 +0500

    Start working on the Generator

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -1,9 +1,17 @@
-
+import Title from "../components/Title"
 
 const Generator = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen text-white p-6 md-12 mt-22">
+      <form className="max-w-4xl mx-auto mb-40">
+      <Title  heading= 'Create the Mamba Image' description="Lets make something beast"/> 
+
+      <div>
+              <div><p>Left </p></div>
+              <div><p>Right </p></div>
+      </div>
+
+      </form>
     </div>
   )
 }
