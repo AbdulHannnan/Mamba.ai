e66dcbbfb39e55523d01de0ec2dd4350fe9de048
Abdul Hannan <ahk12067@gmail.com>
2026-03-17 02:01:10 +0500

    Make left and Right Struture

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -6,8 +6,18 @@ const Generator = () => {
       <form className="max-w-4xl mx-auto mb-40">
       <Title  heading= 'Create the Mamba Image' description="Lets make something beast"/> 
 
-      <div>
-              <div><p>Left </p></div>
+      <div className="flex gap-20 mx-sm:flex-col items-start justify-between">
+
+        {/* left column */}
+              <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
+             
+                <p>Product Image </p>
+                <p>Model Image </p>
+                
+                </div>
+
+
+          {/* right column     */}
               <div><p>Right </p></div>
       </div>
 
