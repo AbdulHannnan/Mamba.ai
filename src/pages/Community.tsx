6562a704eb65b3f3c8fa1a104e77d710908984ad
Abdul Hannan <ahk12067@gmail.com>
2026-03-28 23:52:45 +0500

    Starting the work on the new page

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,4 +1,7 @@
+import  { useState } from 'react'
+
 const Community = () => {
+  const [Projects , setProjects] = useState<>([]);
   return (
     <div>
       
--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,30 @@ export interface UploadZoneProps {
     onClear: () => void;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+export interface User {
+    id? : String;
+    name? : String;
+    email? : String;
+
+}
+
+export interface Project {
+    id: String;
+    name: String;
+    userId: String;
+    user?: User;
+    productName : String
+    productDescription : String;
+    userPrompt : String;
+    aspectRatio : String;
+    targetLeangth? : number;
+    generatedImage?: String;
+    generatedVideo?: String;
+    isGenerating: boolean;
+    isPublished: boolean;
+    error?: String;
+    createdAt?: Date | String;
+    updatedAt?: Date | String;
+
+}
