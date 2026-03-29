104d7cf62f74ddca3044c986d6b6735ac5ad040f
Abdul Hannan <ahk12067@gmail.com>
2026-03-29 23:45:53 +0500

    Make the layout System for the community Page

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,10 +1,31 @@
-import  { useState } from 'react'
+import  { useEffect, useState } from 'react'
+import type { Project } from '../types';
+import { dummyGenerations } from '../assets/assets';
+import { div } from 'framer-motion/client';
+import { Loader2Icon } from 'lucide-react';
 
 const Community = () => {
-  const [Projects , setProjects] = useState<>([]);
-  return (
-    <div>
+  const [Projects , setProjects] = useState<Project[]>([]);
+  const [Loading, setLoading] = useState(true);
+
+  const fetchProjects = async () => {
+  setTimeout(() => { setProjects(dummyGenerations); setLoading(false) }, 3000)
+  }
+
+  useEffect(() => {
+    fetchProjects();
+  }, [])
+
+  return Loading ?(
+    <div className='flex items-center justify-center min-h-screen'>
+      <Loader2Icon className='size-7 animate-spin text-indigo-400'/>
       
+    </div>
+  ) : (
+
+    <div className='min-h-screen text-white p-6 md:p-12 my-20'>
+      <div className='max-w-6xl mx-auto'></div>
+
     </div>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -31,5 +31,6 @@ export interface Project {
     error?: String;
     createdAt?: Date | String;
     updatedAt?: Date | String;
+    uploadImages: string[];
 
 }
