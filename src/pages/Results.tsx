8511b92fed27e133342d8b55271648a602de03d5
Abdul Hannan <ahk12067@gmail.com>
2026-04-24 16:45:42 +0500

    Added the cards details to result page

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Project } from "../types";
 import { dummyGenerations } from "../assets/assets";
-import { Loader2Icon } from "lucide-react";
+import { Loader2Icon, RefreshCwIcon } from "lucide-react";
+import { img } from "framer-motion/client";
 
 const Results = () => {
 
@@ -26,7 +27,50 @@ const Results = () => {
       
     </div>
   ) : (
-    <div></div>
+    <div className="min-h-screen text-white p-6 md:p-12 mt-20">
+      <div className="max-w-6xl mx-auto">
+        <header className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl md:text-3xl font-medium ">Gererato</h1>
+          <link to="/generate" className="btn-secondery text-sm flex items-center gap-2">
+          <RefreshCwIcon className="w-h h-4 " />
+          <p>New generation</p>
+          </link>
+        </header>
+
+        {/* {gril} */}
+       <div className="grid lg:grid-cols-3 gap-8">
+  {/* Main Result Display */}
+  <div className="lg:col-span-2 space-y-6">
+    <div className="glass-panel inline-block p-2 rounded-2xl">
+      <div className={`${projectData?.aspectRatio === "9:16" ? 'aspect-[9/16]' : 'aspect-video'} sm:max-h-200 rounded-xl bg-gray-900 overflow-hidden relative`}>
+        {projectData?.generatedVideo ? (
+          <video 
+            src={projectData.generatedVideo} 
+            controls 
+            autoPlay 
+            loop 
+            className="w-full h-full object-cover"
+          />
+        ) : (
+          <img 
+            src={projectData?.generatedImage} 
+            alt="Generated content"
+            className="w-full h-full object-cover" 
+          />
+        )}
+      </div>
+    </div>
+  </div>
+
+  {/* Sidebar Action */}
+  <div className="lg:col-span-1">
+     {/* Content goes here */}
+  </div>
+</div>
+
+      </div>
+
+    </div>
   )
 }
 
