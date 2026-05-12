83402bcba9d88516988ce9220c011d2fe75f098e
Abdul Hannan <ahk12067@gmail.com>
2026-05-13 00:31:00 +0500

    Fix the Typo error of Sentry Error Tracking

--- BackEnd/Middlewares/Auth.ts
+++ BackEnd/Middlewares/Auth.ts
@@ -1,5 +1,5 @@
 import {Request , Response , NextFunction} from "express";
-import * as Sentry from "@sentry/node"
+import * as Sentry from "@sentry/node";
 
 export const protect = async(req:Request, res: Response, next: NextFunction)=>{
     try {
@@ -9,7 +9,7 @@ export const protect = async(req:Request, res: Response, next: NextFunction)=>{
         }
         next();
     } catch (error: any) {
-        sentary.captureException(error);
+        Sentry.captureException(error);
         res.status(401).json({message: error.code || error.message});
     }
 } 
