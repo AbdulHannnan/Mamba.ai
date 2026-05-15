3122ee76b47e50ccf9276df8ca13b3a67bfb326c
Abdul Hannan <ahk12067@gmail.com>
2026-05-15 15:40:49 +0500

    USer credit function added

---
+++ BackEnd/controllers/UserController.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from 'express';
+import * as Sentry from "@sentry/node";
+
+
+// Get USER Credits 
+export const getUserProfile = async (req: Request, res: Response) => {
+     try {
+        
+    }catch (error: any) {
+        Sentry.captureException(error);
+        res.status(500).json({message: error.code || error.message});
+    }
+}
+
+
+// Get all User Projects
+
