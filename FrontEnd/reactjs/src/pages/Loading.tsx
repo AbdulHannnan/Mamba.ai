886238930d23616ddb4dd682d03c60b7fc4afac2
Abdul Hannan <ahk12067@gmail.com>
2026-06-25 18:55:39 +0500

    Change the Loading Buffer Color Gradient

--- FrontEnd/reactjs/src/pages/Loading.tsx
+++ FrontEnd/reactjs/src/pages/Loading.tsx
@@ -12,7 +12,7 @@ const Loading = () => {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex items-center justify-center flex-1">
-        <Loader2Icon className="size-2 animate-spin text-indigo-200"/>
+        <Loader2Icon className="size-2 animate-spin text-indigo-300"/>
       </div>
     </div>
   )
