f119458844c6c57fd4fb675d327fdc78a3e5a521
Abdul Hannan <ahk12067@gmail.com>
2026-07-11 16:02:57 +0500

    Fix the Route Files

--- BackEnd/routes/projectRoutes.ts
+++ BackEnd/routes/projectRoutes.ts
@@ -4,9 +4,10 @@ const projectRouter = express.Router();
 
 import { createProject, createVideo, getAllPublishedProjects, deletProject } from "../controllers/projectController.js";
 import { protect } from "../Middlewares/Auth.js";
+import upload from "../configs/Multer.js";
 
-projectRouter.post("/create", protect , createProject);
-projectRouter.post("/create-video", protect, createVideo);
+projectRouter.post("/create", protect, upload.array("images", 2), createProject);
+projectRouter.post("/video", protect, createVideo);
 projectRouter.get("/published", getAllPublishedProjects);
 projectRouter.delete("/:projectId", protect, deletProject);
 
